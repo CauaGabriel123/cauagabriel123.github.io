ac823e20f2ace8bcca61e9d58d3abf22697c0210
CauaGabriel123 <caua10012006@gmail.com>
2025-10-19 23:58:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ function buildCatalogAndRender(data) {
 }
 
 (function loadProducts() {
-  fetch('./products_v2.json?nocache=' + Date.now())
+  fetch('https://cauagabriel123.github.io/products_v2.json?nocache=' + Date.now())
     .then(res => {
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
       return res.json();
