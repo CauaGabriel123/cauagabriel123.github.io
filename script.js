b301092b5fa668b663e49383775d79275cf981ed
CauaGabriel123 <caua10012006@gmail.com>
2025-11-07 01:47:48 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1742,7 +1742,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const banners = [
       { src: 'https://cauagabriel123.github.io/assets/rodape_3.PNG', alt: 'Banner 1 LS Store' },
       { src: 'https://cauagabriel123.github.io/assets/rodape_2.PNG', alt: 'Banner 2 LS Store' },
-      { src: 'https://via.placeholder.com/1200x600.png?text=Banner+3', alt: 'Banner 3 LS Store' },
+      { src: 'https://cauagabriel123.github.io/assets/rodape_4', alt: 'Banner 3 LS Store' },
     ];
 
     // monta os slides e os pontinhos
